5dece604b78b7bb467211fd0c6fc31b3ed337952
otenim <snake_istobelieve@keio.jp>
2017-11-30 16:12:01 +0900

    add

--- anomaly_detect2.py
+++ anomaly_detect2.py
@@ -6,58 +6,84 @@ import argparse
 import tqdm
 from PIL import Image
 
+parser = argparse.ArgumentParser()
+parser.add_argument('dataset_normal',choices=['mnist','fashion'])
+parser.add_argument('dataset_anomal',choices=['mnist','fashion'])
+parser.add_argument('--k',type=float,default=3.)
+parser.add_argument('--units',type=int,default=1024)
+parser.add_argument('--batch_size',type=int,default=32)
+parser.add_argument('--loss',choices=['mean_squared_error','l1_error'],default='mean_squared_error')
+parser.add_argument('--activation',choices=['sigmoid','relu'],default='sigmoid')
+
 def add_gaussian_noise(x, mean=0., sigma=0.4):
     gauss = np.random.normal(mean, sigma, size=x.shape)
     x += gauss
     return np.clip(x, 0., 1.)
 
-def main():
+def main(args):
 
-    dataset = datasets.get_dataset('mnist')
-    dataset_sub = datasets.get_dataset('fashion')
-    (x_train, y_train), (x_test, y_test) = dataset.load_data()
-    (x_train_sub, y_train_sub), (x_test_sub, y_test_sub) = dataset_sub.load_data()
-
-    # separate x_test into two dataset, normal and anomal
-    num_anomal = 1000
-    x_test_anomal = x_test_sub[:num_anomal]
-    # x_test_anomal = add_gaussian_noise(x_test[:num_anomal])
-    x_test_normal = x_test[num_anomal:]
-
-    # separate x_train into two dataset, for seq_train and for init_train
-    border = int(1.1 * 1024)
-    x_train_init = x_train[:border]
-    x_train_seq = x_train[border:]
+    dataset_normal = datasets.get_dataset(args.dataset_normal)
+    dataset_anomal = datasets.get_dataset(args.dataset_anomal)
+    (x_train_normal, _), (x_test_normal, _) = dataset_normal.load_data()
+    (_, _), (x_test_anomal, _) = dataset_anomal.load_data()
+    border = int(1.1 * args.units)
+    x_train_normal_init = x_train_normal[:border]
+    x_train_normal_seq = x_train_normal[border:]
 
     # instantiate model
     os_elm = models.OS_ELM(
-        inputs=dataset.inputs,
-        units=1024,
-        outputs=dataset.inputs,
-        loss='mean_squared_error',
-        activation='sigmoid')
+        inputs=dataset_normal.inputs,
+        units=args.units,
+        outputs=dataset_normal.inputs,
+        loss=args.loss,
+        activation=args.activation)
 
     # initial training
     print('now initial training phase...')
-    os_elm.init_train(x_train_init, x_train_init)
+    os_elm.init_train(x_train_normal_init, x_train_normal_init)
 
     # sequential training
     print('now sequential training phase...')
-    batch_size = 256
-    pbar = tqdm.tqdm(total=len(x_train_seq))
-    for i in range(0,len(x_train_seq),batch_size):
-        x_batch = x_train_seq[i:i+batch_size]
+    pbar = tqdm.tqdm(total=len(x_train_normal_seq))
+    for i in range(0,len(x_train_normal_seq),args.batch_size):
+        x_batch = x_train_normal_seq[i:i+args.batch_size]
         os_elm.seq_train(x_batch,x_batch)
         pbar.update(n=len(x_batch))
     pbar.close()
 
     # test
     print('now test phase...')
-    loss_normal = os_elm.compute_loss(x_test_normal,x_test_normal)
-    loss_anomal = os_elm.compute_loss(x_test_anomal,x_test_anomal)
+    losses_normal = []
+    losses_anomal = []
+    for x in x_test_normal:
+        losses_normal.append(os_elm.compute_loss(x,x))
+    for x in x_test_anomal:
+        losses_anomal.append(os_elm.compute_loss(x,x))
+    loss_normal = np.mean(losses_normal)
+    loss_anomal = np.mean(losses_anomal)
     print('loss_normal: %f' % (loss_normal))
     print('loss_anomal: %f' % (loss_anomal))
 
+    losses = np.concatenate((losses_normal,losses_anomal),axis=0)
+    labels = np.concatenate(
+        (
+            [False for i in range(len(losses_normal))],
+            [True for i in range(len(losses_anomal))]
+        ),
+        axis=0
+    )
+    losses = (losses - loss_normal) / np.std(losses_normal)
+    thr = args.k;
+    TP = np.sum(labels & (losses > thr))
+    precision = TP / np.sum(losses > thr)
+    recall = TP / np.sum(labels)
+    f_measure = (2. * recall * precision) / (recall + precision)
+    print('precision: %f' % precision)
+    print('recall: %f' % recall)
+    print('f_measure: %f' % f_measure)
+
+
 
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    main(args)
