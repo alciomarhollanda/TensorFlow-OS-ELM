c549a81a56b82cd1aed91221bcefc538a56aff9a
otenim <snake_istobelieve@keio.jp>
2018-01-25 20:41:16 +0900

    added

--- eval_predict_batch.sh
+++ eval_predict_batch.sh
@@ -5,6 +5,6 @@ do
     for batch_size in 4 8 16 32 64 128 256
     do
         echo "********** units:$units, batch_size:$batch_size"
-        eval "python predict.py --iterations 500 --inputs 784 --units $units --outputs 784 --batch_size $batch_size"
+        eval "CUDA_VISIBLE_DEVICES=-1 python predict_batch.py --iterations 500 --inputs 784 --units $units --outputs 784 --batch_size $batch_size"
     done
 done
