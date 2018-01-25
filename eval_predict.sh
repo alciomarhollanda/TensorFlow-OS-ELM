8ce70c6493c0159225b39e86435253660e0182a0
otenim <snake_istobelieve@keio.jp>
2018-01-25 23:08:16 +0900

    added

--- eval_predict.sh
+++ eval_predict.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-for units in 32 64 128
+for units in 8 16 32
 do
     for batch_size in 4 8 16 32 64 128 256
     do
         echo "********** units:$units, batch_size:$batch_size"
-        eval "python predict.py --iterations 500 --inputs 784 --units $units --outputs 784 --batch_size $batch_size"
+        eval "python predict.py --iterations 2000 --inputs 64 --units $units --outputs 64 --batch_size $batch_size"
     done
 done
